cde625ff23ca78d746385b11583805cc2272fae8
kzmolnar96 <kzmolnar96@gmail.com>
2017-11-27 17:04:28 -0500

    Interactable
    Interactable

---
+++ action-potential-unity/Assets/scripts/TagOnOff.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var thingOn : GameObject;
+var thingOff : GameObject;
+var objectTag: String;
+var duration : float = 1;
+
+
+function OnTriggerEnter (other : Collider) {
+    if(other.gameObject.tag == objectTag){
+        yield WaitForSeconds(duration);
+        thingOn.SetActive(true);
+        thingOff.SetActive(false);
+    }
+}
---
+++ action-potential-unity/Assets/scripts/TagTransform.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+var thingtomove : GameObject;
+var objectTag : String;
+var startPoint : Vector3;
+var endPoint : Vector3;
+var duration : float = 1;
+//var yourVar = false;
+
+
+//private var startPoint : Vector3;
+
+private var startTime : float;
+private var yourVar = false;
+
+//function Start() {
+//   startPoint = transform.position;
+// startTime = Time.time;
+//}
+
+function OnTriggerEnter (other : Collider) {
+    if(other.gameObject.tag == objectTag){
+        yourVar = true;
+        startTime = Time.time;
+    }
+}
+
+function Update () {
+    if(yourVar == true){
+        thingtomove.transform.position = Vector3.Lerp(startPoint, endPoint, (Time.time - startTime) / duration);
+        if(thingtomove.transform.position == endPoint) {
+            startTime = Time.time;
+            thingtomove.transform.position = startPoint;
+        }
+    }
+}
